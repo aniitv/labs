326fc2422a7e03d84dd6ab68844865b74103da3f
aniitv <anastasiiaazotova@gmail.com>
2025-10-20 20:14:24 +0300

    Add files via upload

---
+++ lab3/2-key.js
@@ -0,0 +1,12 @@
+const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ@#$%^&*()-_=+[]{}|;:,.<>?';
+
+function generateKey(length) {
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        result += characters[randomIndex];
+    }
+    return result;
+}
+
+console.log(generateKey(10));
