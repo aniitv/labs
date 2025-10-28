d450bacb99536f60902c755428361f7839b04117
aniitv <anastasiiaazotova@gmail.com>
2025-10-28 17:45:19 +0200

    Update 3-ip.js

--- lab3/3-ip.js
+++ lab3/3-ip.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 const ipToInt = (ip = '127.0.0.1') => {
     return ip
@@ -9,15 +8,4 @@ const ipToInt = (ip = '127.0.0.1') => {
 };
 
 console.log(ipToInt('127.0.0.1'));  
-=======
 
-const ipToInt = (ip = '127.0.0.1') => {
-    return ip
-        .split('.')
-        .map(Number)
-        .reduce((acc, val) =>
-            (acc << 8) + val, 0);
-};
-
-console.log(ipToInt('127.0.0.1'));  
->>>>>>> 90a40211909d75e6ec83ef821dfaef3fca795b0a
