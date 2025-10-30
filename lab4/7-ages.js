a1a29dd0023dc43aebbe0a76e65bd5dc82617a71
Anastasiia Zotova <anastasiiaazotova@gmail.com>
2025-10-30 22:10:34 +0200

    add 7-ages.js to lab4

--- lab4/7-ages.js
+++ lab4/7-ages.js
@@ -1,8 +1,20 @@
-// const persons = {
-//   lenin: { born: 1870, died: 1924 },
-//   mao: { born: 1893, died: 1976 },
-//   gandhi: { born: 1869, died: 1948 },
-//   hirohito: { born: 1901, died: 1989 },
-// };
-// for person i persons
-// console.log(ages(persons));
+const ages = (persons) => {
+  const lifeAges = {};
+
+  for (const personName in persons) {
+    const birthDate = persons[personName].born;
+    const deathDate = persons[personName].died;
+
+    const age = deathDate - birthDate;
+    lifeAges[personName] = age;
+  }
+
+  return lifeAges;
+};
+const persons = {
+  lenin: { born: 1870, died: 1924 },
+  mao: { born: 1893, died: 1976 },
+  gandhi: { born: 1869, died: 1948 },
+  hirohito: { born: 1901, died: 1989 },
+};
+console.log(ages(persons));
